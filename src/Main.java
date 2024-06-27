440cb1b00edb3dac72039fcfadf49e7dd2e3b0f3
Hirrad <haddi.luhrasbe@gmail.com>
2024-06-27 19:18:44 +0330

    Added ReadLine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Reading data using readLine
+        try {
+            var name = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
